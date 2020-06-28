67b67a7566c88d71ce27b0ac1d70a60f42f357f7
rohitv450 <67551767+rohitv450@users.noreply.github.com>
2020-06-29 00:41:37 +0530

    Demo commit and first one

---
+++ src/MainDemo.java
@@ -0,0 +1,19 @@
+
+public class MainDemo {
+	
+
+
+		private String name;
+		
+		public String getName(String s) {
+			return name;
+			
+		}
+		
+		public void setName() {
+			
+			this.name=name;
+		}
+		
+	
+}
